2b8b0aad64dba542aeac3d763079e61dfc45bcab
Michelle Tambi <michelletambi22@gmail.com>
2022-04-19 16:42:06 +0100

    completed exercises

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -17,13 +17,24 @@ public class Runner {
 //        1. Add "Coll" to the end of the list
         scottishIslands.add("Coll");
 //        2. Add "Tiree" to the start of the list
+        scottishIslands.add(0, "Tiree");
 //        3. Add "Islay" after "Jura" and before "Mull"
+        scottishIslands.add(2, "Islay");
 //        4. Print out the index position of "Skye"
+        System.out.println(scottishIslands.indexOf("Skye"));
 //        5. Remove "Tresco" from the list by name
+        scottishIslands.remove("Tresco");
 //        6. Remove "Arran" from the list by index
+        scottishIslands.remove(5);
 //        7. Print the number of islands in your arraylist
+        System.out.println(scottishIslands.size());
 //        8. Sort the list alphabetically
+        Collections.sort(scottishIslands);
 //        9. Print out all the islands using a for loop
+        for (String island : scottishIslands) {
+            System.out.println(island);
+        }
+
 
         System.out.println(scottishIslands);
 
@@ -34,16 +45,38 @@ public class Runner {
         System.out.println("numbers: " + numbers);
 
 //        1. Print out a list of the even integers
+        for (int t = 0; t < numbers.size(); t ++){
+            if (numbers.get(t) % 2 == 0) {
+                System.out.println(numbers.get(t));
+            }
+        }
 //        2. Print the difference between the largest and smallest value
+        int difference = Collections.max(numbers) - Collections.min(numbers);
+        System.out.println(difference);
 //        3. Print true if the list contains a 1 next to a 1 somewhere
+
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) == 1 && numbers.get(i + 1) == 1) {
+                System.out.println("true");
+                break;
+            }
+        }
+
 //        4. Print the sum of the numbers
+            int rolling = 0;
+            for (int j = 0; j < numbers.size(); j++) {
+                rolling += numbers.get(j);
+            }
+            System.out.println(rolling);
 //        5. Print the sum of the numbers...
 //           ...except the number 13 is unlucky, so it does not count...
 //           ...and numbers that come immediately after a 13 also do not count
 //          HINT - You will need to track the index throughout the loop
 //
 //          So [7, 13, 2] would have sum of 9.
+            
+
+        }
 
     }
 
-}
